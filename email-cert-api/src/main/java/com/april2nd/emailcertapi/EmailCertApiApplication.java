709931d7aae460866fd23656218c9ecf7c0c6a94
7unho <rlawnsgh8395@naver.com>
2023-01-12 22:09:46 +0900

    feat: project init

---
+++ email-cert-api/src/main/java/com/april2nd/emailcertapi/EmailCertApiApplication.java
@@ -0,0 +1,13 @@
+package com.april2nd.emailcertapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailCertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailCertApiApplication.class, args);
+	}
+
+}
---
+++ email-cert-api/src/test/java/com/april2nd/emailcertapi/EmailCertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.april2nd.emailcertapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailCertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
