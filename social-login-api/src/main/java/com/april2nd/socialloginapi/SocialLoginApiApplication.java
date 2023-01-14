4ec1a6898176b35b801a0ed4912c8c180d80ef85
7unho <rlawnsgh8395@naver.com>
2023-01-14 23:07:39 +0900

    feat: social-login-api init

---
+++ social-login-api/src/main/java/com/april2nd/socialloginapi/SocialLoginApiApplication.java
@@ -0,0 +1,13 @@
+package com.april2nd.socialloginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLoginApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLoginApiApplication.class, args);
+    }
+
+}
---
+++ social-login-api/src/test/java/com/april2nd/socialloginapi/SocialLoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.april2nd.socialloginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLoginApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
